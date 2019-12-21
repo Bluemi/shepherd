3e4baca510bd5ae54362fc029d5e0b9b36912d07
Bluemi <bruno.schilling@protonmail.ch>
2019-12-21 19:32:55 +0100

    removed player::render()

--- src/common/player.cpp
+++ src/common/player.cpp
@@ -5,21 +5,6 @@
 
 player::player(unsigned int id, const std::string& name): _id(id), _name(name) {}
 
-/*
-void player::render(const shader_program& shader_program) {
-	shader_program.use();
-	_shape.bind();
-
-	glm::mat4 model = glm::mat4(1.f);
-	model = glm::translate(model, _position);
-	shader_program.set_4fv("model", model);
-
-	shader_program.set_3f("color", glm::vec3(0.2, 0.2, 0.5));
-
-	glDrawArrays(GL_TRIANGLES, 0, _shape.get_number_vertices());
-}
-*/
-
 unsigned int player::get_id() const {
 	return _id;
 }
