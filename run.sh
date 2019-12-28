688c1c3ad6b55149d69c3819407440986dd4c1eb
Bluemi <bruno.schilling@protonmail.ch>
2019-12-28 21:27:36 +0100

    ...

--- run.sh
+++ run.sh
@@ -4,6 +4,8 @@ if [ "$1" == "s" ]; then
 	./build/bin/server
 elif [ "$1" == "t" ]; then
 	./build/tests/bin/packet_helper_test
+elif [ "$1" == "r" ]; then
+	./build/bin/client "192.168.178.89" "player0"
 else
 	./build/bin/client "localhost" "player0"
 fi
